28c743784322b87aeb9a9fc2bd113377ff6ff2e4
Edy Rodriguez <zeronoaz@gmail.com>
2023-04-03 03:36:35 -0600

    fix: errors on the serverless function

--- api/index.mjs
+++ api/index.mjs
@@ -1,8 +1,10 @@
-const fetch = require('node-fetch');
+import fetch from 'node-fetch';
 
-module.exports = async (req, res) => {
-  // Check if the url query parameter is provided
+export default async (req, res) => {
+  console.log('CORS proxy function called with query:', req.query);
+
   if (!req.query.url) {
+    console.error('Missing "url" query parameter');
     return res.status(400).send('Missing "url" query parameter');
   }
 
@@ -10,7 +12,6 @@ module.exports = async (req, res) => {
     const response = await fetch(req.query.url);
     const data = await response.text();
 
-    // Set CORS headers
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader(
@@ -18,15 +19,14 @@ module.exports = async (req, res) => {
       'Content-Type, Authorization, X-Requested-With'
     );
 
-    // Forward the response status and headers
     res.status(response.status);
     for (const [key, value] of response.headers.entries()) {
       res.setHeader(key, value);
     }
 
-    // Send the response data
     res.send(data);
   } catch (error) {
+    console.error('Error fetching data from the URL:', error);
     res.status(500).send(`Error fetching data from the URL: ${error.message}`);
   }
 };
